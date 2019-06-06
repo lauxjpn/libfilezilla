6437a57667ad807d4a6c167604cac2e01a9369e6
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-06-06 14:31:04 +0000

    Add translation functions.

---
+++ libfilezilla/trunk/lib/libfilezilla/translate.hpp
@@ -0,0 +1,33 @@
+#ifndef LIBFILEZILLA_TRANSLATE_HEADER
+#define LIBFILEZILLA_TRANSLATE_HEADER
+
+#include <string>
+
+/** \file
+ * \brief Functions to translate strings
+ */
+namespace fz {
+
+/** \brief Sets translators for strings
+ *
+ * You could pass functions that in turn call gettext/ngettext
+ */
+void set_translators(
+	std::wstring(*s) (char const* const t),
+	std::wstring(*pf)(char const* const singular, char const* const plural, int64_t n)
+);
+
+/** \brief Translates the input string with the configured translator.
+ *
+ * Returns the untranslated string is if no translator has previously been configured
+ */
+std::wstring translate(char const* const source);
+std::wstring translate(char const* const singular, char const * const plural, int64_t n);
+}
+
+// Sadly xgettext cannot be used with namespaces
+#define fztranslate fz::translate
+#define fztranslate fz::translate
+#define fztranslate_mark
+
+#endif
