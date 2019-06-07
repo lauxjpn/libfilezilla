c86b07a35ff3680c946cb156b93972c3bfd66359
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-06-07 14:48:04 +0000

    Faster error lookup

--- lib/socket_errors.cpp
+++ lib/socket_errors.cpp
@@ -12,6 +12,8 @@
 #include <netdb.h>
 #endif
 
+#include <unordered_map>
+
 // Fixups needed on FreeBSD
 #if !defined(EAI_ADDRFAMILY) && defined(EAI_FAMILY)
   #define EAI_ADDRFAMILY EAI_FAMILY
@@ -19,115 +21,123 @@
 
 namespace fz {
 
-#define ERRORDECL(c, desc) { c, #c, desc },
-
 namespace {
-struct Error_table
+struct Error
 {
-	int code;
-	char const* const name;
-	char const* const description;
+	std::string name;
+	std::string description;
 };
 
-static Error_table const error_table[] =
+std::unordered_map<int, Error> const& get_errors()
 {
-	ERRORDECL(EACCES, fztranslate_mark("Permission denied"))
-	ERRORDECL(EADDRINUSE, fztranslate_mark("Local address in use"))
-	ERRORDECL(EAFNOSUPPORT, fztranslate_mark("The specified address family is not supported"))
-	ERRORDECL(EINPROGRESS, fztranslate_mark("Operation in progress"))
-	ERRORDECL(EINVAL, fztranslate_mark("Invalid argument passed"))
-	ERRORDECL(EMFILE, fztranslate_mark("Process file table overflow"))
-	ERRORDECL(ENFILE, fztranslate_mark("System limit of open files exceeded"))
-	ERRORDECL(ENOBUFS, fztranslate_mark("Out of memory"))
-	ERRORDECL(ENOMEM, fztranslate_mark("Out of memory"))
-	ERRORDECL(EPERM, fztranslate_mark("Permission denied"))
-	ERRORDECL(EPROTONOSUPPORT, fztranslate_mark("Protocol not supported"))
-	ERRORDECL(EAGAIN, fztranslate_mark("Resource temporarily unavailable"))
-	ERRORDECL(EALREADY, fztranslate_mark("Operation already in progress"))
-	ERRORDECL(EBADF, fztranslate_mark("Bad file descriptor"))
-	ERRORDECL(ECONNREFUSED, fztranslate_mark("Connection refused by server"))
-	ERRORDECL(EFAULT, fztranslate_mark("Socket address outside address space"))
-	ERRORDECL(EINTR, fztranslate_mark("Interrupted by signal"))
-	ERRORDECL(EISCONN, fztranslate_mark("Socket already connected"))
-	ERRORDECL(ENETUNREACH, fztranslate_mark("Network unreachable"))
-	ERRORDECL(ENOTSOCK, fztranslate_mark("File descriptor not a socket"))
-	ERRORDECL(ETIMEDOUT, fztranslate_mark("Connection attempt timed out"))
-	ERRORDECL(EHOSTUNREACH, fztranslate_mark("No route to host"))
-	ERRORDECL(ENOTCONN, fztranslate_mark("Socket not connected"))
-	ERRORDECL(ENETRESET, fztranslate_mark("Connection reset by network"))
-	ERRORDECL(EOPNOTSUPP, fztranslate_mark("Operation not supported"))
-	ERRORDECL(ESHUTDOWN, fztranslate_mark("Socket has been shut down"))
-	ERRORDECL(EMSGSIZE, fztranslate_mark("Message too large"))
-	ERRORDECL(ECONNABORTED, fztranslate_mark("Connection aborted"))
-	ERRORDECL(ECONNRESET, fztranslate_mark("Connection reset by peer"))
-	ERRORDECL(EPIPE, fztranslate_mark("Local endpoint has been closed"))
-	ERRORDECL(EHOSTDOWN, fztranslate_mark("Host is down"))
+	static std::unordered_map<int, Error> const errors = [](){
+		std::unordered_map<int, Error> ret;
 
-	// Getaddrinfo related
-#ifdef EAI_ADDRFAMILY
-	ERRORDECL(EAI_ADDRFAMILY, fztranslate_mark("Network host does not have any network addresses in the requested address family"))
-#endif
-	ERRORDECL(EAI_AGAIN, fztranslate_mark("Temporary failure in name resolution"))
-	ERRORDECL(EAI_BADFLAGS, fztranslate_mark("Invalid value for ai_flags"))
-#ifdef EAI_BADHINTS
-	ERRORDECL(EAI_BADHINTS, fztranslate_mark("Invalid value for hints"))
-#endif
-	ERRORDECL(EAI_FAIL, fztranslate_mark("Nonrecoverable failure in name resolution"))
-	ERRORDECL(EAI_FAMILY, fztranslate_mark("The ai_family member is not supported"))
-	ERRORDECL(EAI_MEMORY, fztranslate_mark("Memory allocation failure"))
-#ifdef EAI_NODATA
-	ERRORDECL(EAI_NODATA, fztranslate_mark("No address associated with nodename"))
-#endif
-	ERRORDECL(EAI_NONAME, fztranslate_mark("Neither nodename nor servname provided, or not known"))
-#ifdef EAI_OVERFLOW
-	ERRORDECL(EAI_OVERFLOW, fztranslate_mark("Argument buffer overflow"))
-#endif
-#ifdef EAI_PROTOCOL
-	ERRORDECL(EAI_PROTOCOL, fztranslate_mark("Resolved protocol is unknown"))
-#endif
-	ERRORDECL(EAI_SERVICE, fztranslate_mark("The servname parameter is not supported for ai_socktype"))
-	ERRORDECL(EAI_SOCKTYPE, fztranslate_mark("The ai_socktype member is not supported"))
-#ifdef EAI_SYSTEM
-	ERRORDECL(EAI_SYSTEM, fztranslate_mark("Other system error"))
-#endif
-#ifdef EAI_IDN_ENCODE
-	ERRORDECL(EAI_IDN_ENCODE, fztranslate_mark("Invalid characters in hostname"))
-#endif
+		auto sorted_insert = [&ret](int code, std::string const& name, std::string const& description)
+		{
+			auto it = ret.find(code);
+			if (it == ret.cend()) {
+				ret[code] = {name, description};
+			}
+		};
+		#define insert(c, desc) sorted_insert(c, #c, desc)
 
-	// Codes that have no POSIX equivalence
-#ifdef FZ_WINDOWS
-	ERRORDECL(WSANOTINITIALISED, fztranslate_mark("Not initialized, need to call WSAStartup"))
-	ERRORDECL(WSAENETDOWN, fztranslate_mark("System's network subsystem has failed"))
-	ERRORDECL(WSAEPROTOTYPE, fztranslate_mark("Protocol not supported on given socket type"))
-	ERRORDECL(WSAESOCKTNOSUPPORT, fztranslate_mark("Socket type not supported for address family"))
-	ERRORDECL(WSAEADDRNOTAVAIL, fztranslate_mark("Cannot assign requested address"))
-	ERRORDECL(ERROR_NETNAME_DELETED, fztranslate_mark("The specified network name is no longer available"))
-#endif
-	{ 0, nullptr, nullptr }
-};
+		insert(EACCES, fztranslate_mark("Permission denied"));
+		insert(EADDRINUSE, fztranslate_mark("Local address in use"));
+		insert(EAFNOSUPPORT, fztranslate_mark("The specified address family is not supported"));
+		insert(EINPROGRESS, fztranslate_mark("Operation in progress"));
+		insert(EINVAL, fztranslate_mark("Invalid argument passed"));
+		insert(EMFILE, fztranslate_mark("Process file table overflow"));
+		insert(ENFILE, fztranslate_mark("System limit of open files exceeded"));
+		insert(ENOBUFS, fztranslate_mark("Out of memory"));
+		insert(ENOMEM, fztranslate_mark("Out of memory"));
+		insert(EPERM, fztranslate_mark("Permission denied"));
+		insert(EPROTONOSUPPORT, fztranslate_mark("Protocol not supported"));
+		insert(EAGAIN, fztranslate_mark("Resource temporarily unavailable"));
+		insert(EALREADY, fztranslate_mark("Operation already in progress"));
+		insert(EBADF, fztranslate_mark("Bad file descriptor"));
+		insert(ECONNREFUSED, fztranslate_mark("Connection refused by server"));
+		insert(EFAULT, fztranslate_mark("Socket address outside address space"));
+		insert(EINTR, fztranslate_mark("Interrupted by signal"));
+		insert(EISCONN, fztranslate_mark("Socket already connected"));
+		insert(ENETUNREACH, fztranslate_mark("Network unreachable"));
+		insert(ENOTSOCK, fztranslate_mark("File descriptor not a socket"));
+		insert(ETIMEDOUT, fztranslate_mark("Connection attempt timed out"));
+		insert(EHOSTUNREACH, fztranslate_mark("No route to host"));
+		insert(ENOTCONN, fztranslate_mark("Socket not connected"));
+		insert(ENETRESET, fztranslate_mark("Connection reset by network"));
+		insert(EOPNOTSUPP, fztranslate_mark("Operation not supported"));
+		insert(ESHUTDOWN, fztranslate_mark("Socket has been shut down"));
+		insert(EMSGSIZE, fztranslate_mark("Message too large"));
+		insert(ECONNABORTED, fztranslate_mark("Connection aborted"));
+		insert(ECONNRESET, fztranslate_mark("Connection reset by peer"));
+		insert(EPIPE, fztranslate_mark("Local endpoint has been closed"));
+		insert(EHOSTDOWN, fztranslate_mark("Host is down"));
+
+		// Getaddrinfo related
+	#ifdef EAI_ADDRFAMILY
+		insert(EAI_ADDRFAMILY, fztranslate_mark("Network host does not have any network addresses in the requested address family"));
+	#endif
+		insert(EAI_AGAIN, fztranslate_mark("Temporary failure in name resolution"));
+		insert(EAI_BADFLAGS, fztranslate_mark("Invalid value for ai_flags"));
+	#ifdef EAI_BADHINTS
+		insert(EAI_BADHINTS, fztranslate_mark("Invalid value for hints"));
+	#endif
+		insert(EAI_FAIL, fztranslate_mark("Nonrecoverable failure in name resolution"));
+		insert(EAI_FAMILY, fztranslate_mark("The ai_family member is not supported"));
+		insert(EAI_MEMORY, fztranslate_mark("Memory allocation failure"));
+	#ifdef EAI_NODATA
+		insert(EAI_NODATA, fztranslate_mark("No address associated with nodename"));
+	#endif
+		insert(EAI_NONAME, fztranslate_mark("Neither nodename nor servname provided, or not known"));
+	#ifdef EAI_OVERFLOW
+		insert(EAI_OVERFLOW, fztranslate_mark("Argument buffer overflow"));
+	#endif
+	#ifdef EAI_PROTOCOL
+		insert(EAI_PROTOCOL, fztranslate_mark("Resolved protocol is unknown"));
+	#endif
+		insert(EAI_SERVICE, fztranslate_mark("The servname parameter is not supported for ai_socktype"));
+		insert(EAI_SOCKTYPE, fztranslate_mark("The ai_socktype member is not supported"));
+	#ifdef EAI_SYSTEM
+		insert(EAI_SYSTEM, fztranslate_mark("Other system error"));
+	#endif
+	#ifdef EAI_IDN_ENCODE
+		insert(EAI_IDN_ENCODE, fztranslate_mark("Invalid characters in hostname"));
+	#endif
+
+		// Codes that have no POSIX equivalence
+	#ifdef FZ_WINDOWS
+		insert(WSANOTINITIALISED, fztranslate_mark("Not initialized, need to call WSAStartup"));
+		insert(WSAENETDOWN, fztranslate_mark("System's network subsystem has failed"));
+		insert(WSAEPROTOTYPE, fztranslate_mark("Protocol not supported on given socket type"));
+		insert(WSAESOCKTNOSUPPORT, fztranslate_mark("Socket type not supported for address family"));
+		insert(WSAEADDRNOTAVAIL, fztranslate_mark("Cannot assign requested address"));
+		insert(ERROR_NETNAME_DELETED, fztranslate_mark("The specified network name is no longer available"));
+	#endif
+
+		return ret;
+	}();
+
+	return errors;
+}
 }
 
 std::string socket_error_string(int error)
 {
-	for (int i = 0; error_table[i].code; ++i) {
-		if (error != error_table[i].code) {
-			continue;
-		}
-
-		return error_table[i].name;
+	auto const& errors = get_errors();
+	auto const it = errors.find(error);
+	if (it != errors.end()) {
+		return it->second.name;
 	}
-
 	return to_string(error);
 }
 
 native_string socket_error_description(int error)
 {
-	for (int i = 0; error_table[i].code; ++i) {
-		if (error != error_table[i].code) {
-			continue;
-		}
-
-		return to_native(to_native(std::string(error_table[i].name)) + fzT(" - ") + to_native(translate(error_table[i].description)));
+	auto const& errors = get_errors();
+	auto const it = errors.find(error);
+	if (it != errors.end()) {
+		return to_native(to_native(std::string(it->second.name)) + fzT(" - ") + to_native(translate(it->second.description.c_str())));
 	}
 
 	return sprintf(fzT("%d"), error);
