c9cfd2fa8363939870cf09a46a4fedfc0dc776d5
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-06-11 15:45:23 +0000

    Update libfilezilla highlights in the documentation.

--- libfilezilla/trunk/lib/libfilezilla/libfilezilla.hpp
+++ libfilezilla/trunk/lib/libfilezilla/libfilezilla.hpp
@@ -39,6 +39,8 @@
  *
  * \li A typesafe, multi-threaded event system that's very simple to use yet extremely efficient
  * \li Timers for periodic events
+ * \li A \ref fz::socket class for TCP communication with infrastructure to easily add layers on top
+ * \li A TLS layer to secure communication
  * \li A \ref fz::datetime "datetime" class that not only tracks timestamp but also their accuracy, which simplifies dealing with timestamps originating from different sources
  * \li Simple \ref fz::process "process" handling for spawning child processes with redirected I/O
  *
