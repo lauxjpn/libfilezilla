c8709d408bc83844e510f5b5cb70a60d2b36f37d
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-06-21 11:15:55 +0000

    Add support for detached signatures.

--- libfilezilla/trunk/lib/signature.cpp
+++ libfilezilla/trunk/lib/signature.cpp
@@ -66,40 +66,64 @@ public_verification_key private_signing_key::pubkey() const
 }
 
 
-std::vector<uint8_t> sign(uint8_t const* message, size_t const size, private_signing_key const& priv)
+std::vector<uint8_t> sign(uint8_t const* message, size_t const size, private_signing_key const& priv, bool include_message)
 {
 	std::vector<uint8_t> ret;
 
 	auto const pub = priv.pubkey();
 	if (priv && pub && size) {
-		ret.reserve(size + signature_size);
-		ret.assign(message, message + size);
-		ret.resize(size + signature_size);
+		size_t retsize = signature_size;
+		size_t offset{};
+		if (include_message) {
+			offset = size;
+			retsize += size;
+			ret.reserve(retsize);
+			ret.assign(message, message + size);
+		}
+		ret.resize(retsize);
 
-		nettle_ed25519_sha512_sign(pub.key_.data(), priv.data().data(), size, ret.data(), ret.data() + size);
+		nettle_ed25519_sha512_sign(pub.key_.data(), priv.data().data(), size, message, ret.data() + offset);
 	}
 
 	return ret;
 }
 
-std::vector<uint8_t> sign(std::vector<uint8_t> const& message, private_signing_key const& priv)
+std::vector<uint8_t> sign(std::vector<uint8_t> const& message, private_signing_key const& priv, bool include_message)
 {
-	return sign(message.data(), message.size(), priv);
+	return sign(message.data(), message.size(), priv, include_message);
 }
 
-std::vector<uint8_t> sign(std::string_view const& message, private_signing_key const& priv)
+std::vector<uint8_t> sign(std::string_view const& message, private_signing_key const& priv, bool include_message)
 {
-	return sign(reinterpret_cast<uint8_t const*>(message.data()), message.size(), priv);
+	return sign(reinterpret_cast<uint8_t const*>(message.data()), message.size(), priv, include_message);
 }
 
 
-// Verify the message. Returns true iff it has been signed by the private key corresponding to the passed public key
+
+bool verify(uint8_t const* message, size_t const message_size, uint8_t const* signature, size_t const sig_size, public_verification_key const& pub)
+{
+	if (!message || !signature || sig_size != signature_size) {
+		return false;
+	}
+	return nettle_ed25519_sha512_verify(pub.key_.data(), message_size, message, signature) == 1;
+}
+
+bool verify(std::vector<uint8_t> const& message, std::vector<uint8_t> const& signature, public_verification_key const& pub)
+{
+	return verify(message.data(), message.size(), signature.data(), signature.size(), pub);
+}
+
+bool verify(std::string_view const& message, std::string_view const& signature, public_verification_key const& pub)
+{
+	return verify(reinterpret_cast<uint8_t const*>(message.data()), message.size(), reinterpret_cast<uint8_t const*>(signature.data()), signature.size(), pub);
+}
+
 bool verify(uint8_t const* message, size_t const size, public_verification_key const& pub)
 {
 	if (!message || size < signature_size) {
 		return false;
 	}
-	return nettle_ed25519_sha512_verify(pub.key_.data(), size - signature_size, message, message + size - signature_size) == 1;
+	return verify(message, size - signature_size, message + (size - signature_size), signature_size, pub);
 }
 
 bool verify(std::vector<uint8_t> const& message, public_verification_key const& pub)
--- libfilezilla/trunk/tests/crypto.cpp
+++ libfilezilla/trunk/tests/crypto.cpp
@@ -1,4 +1,5 @@
 #include "libfilezilla/encryption.hpp"
+#include "libfilezilla/signature.hpp"
 #include "libfilezilla/util.hpp"
 
 #include "test_utils.hpp"
@@ -10,6 +11,7 @@ class crypto_test final : public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE(crypto_test);
 	CPPUNIT_TEST(test_encryption);
 	CPPUNIT_TEST(test_encryption_with_password);
+	CPPUNIT_TEST(test_signature);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -18,6 +20,7 @@ public:
 
 	void test_encryption();
 	void test_encryption_with_password();
+	void test_signature();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(crypto_test);
@@ -60,4 +63,47 @@ void crypto_test::test_encryption_with_password()
 		CPPUNIT_ASSERT(fz::decrypt(cipher, priv) == std::vector<uint8_t>(plain.cbegin(), plain.cend()));
 	}
 
+}
+
+void crypto_test::test_signature()
+{
+	// Test privkey generation
+	auto const priv = fz::private_signing_key::generate();
+	CPPUNIT_ASSERT(priv);
+	CPPUNIT_ASSERT(!priv.to_base64().empty());
+	CPPUNIT_ASSERT(fz::private_signing_key::from_base64(priv.to_base64()));
+
+	// Test pubkey generation
+	auto const pub = priv.pubkey();
+	CPPUNIT_ASSERT(pub);
+	CPPUNIT_ASSERT(!pub.to_base64().empty());
+	CPPUNIT_ASSERT(fz::public_verification_key::from_base64(pub.to_base64()));
+
+	// Test signing attached
+	auto sig = fz::sign("Hello", priv);
+	std::string_view sigv(reinterpret_cast<char const*>(sig.data()), sig.size());
+	CPPUNIT_ASSERT(!sig.empty());
+
+	// Test signing detached
+	auto sig2 = fz::sign("Hello", priv, false);
+	std::string_view sig2v(reinterpret_cast<char const*>(sig2.data()), sig2.size());
+	CPPUNIT_ASSERT(!sig2.empty());
+
+	// Test sig verification
+	CPPUNIT_ASSERT(fz::verify(sig, pub));
+	CPPUNIT_ASSERT(fz::verify("Hello", sig2v, pub));
+	CPPUNIT_ASSERT(!fz::verify(sig2, pub));
+	CPPUNIT_ASSERT(!fz::verify("Hello", sigv, pub));
+
+	// Test verification with wrong key fails
+	auto const pub2 = fz::private_signing_key::generate().pubkey();
+	CPPUNIT_ASSERT(pub2);
+	CPPUNIT_ASSERT(!fz::verify(sig, pub2));
+	CPPUNIT_ASSERT(!fz::verify("Hello", sig2v, pub2));
+
+	// Test verification of modified data fails
+	sig[5] ^= 0x2c;
+	sig2[5] ^= 0x2c;
+	CPPUNIT_ASSERT(!fz::verify(sig, pub));
+	CPPUNIT_ASSERT(!fz::verify("Hello", sig2v, pub));
 }
