2a6b6df72bc3f21d0c932b9e24954c6262bef0ae
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-10-15 13:28:20 +0000

    API cleanup.

--- libfilezilla/trunk/tests/ratelimit.cpp
+++ libfilezilla/trunk/tests/ratelimit.cpp
@@ -25,10 +25,10 @@ struct handler : public fz::event_handler
 		sub_limiter_[0].add(&buckets_[4]);
 		sub_limiter_[1].add(&buckets_[5]);
 		sub_limiter_[2].add(&buckets_[6]);
-		limiter_.set_limits(10000, fz::unlimited);
-		sub_limiter_[0].set_limits(1000, fz::unlimited);
+		limiter_.set_limits(10000, fz::rate::unlimited);
+		sub_limiter_[0].set_limits(1000, fz::rate::unlimited);
 
-		sub_limiter_[1].set_limits(2500, fz::unlimited);
+		sub_limiter_[1].set_limits(2500, fz::rate::unlimited);
 	}
 
 	~handler()
@@ -56,13 +56,13 @@ struct handler : public fz::event_handler
 			}
 			std::cout << "Total rate is " << sum / duration << " bytes/s\n";
 
-			float ratio = float(sum) / (limiter_.limit(0) * duration);
+			float ratio = float(sum) / (limiter_.limit(fz::direction::inbound) * duration);
 			if (ratio < 0.9 || ratio > 1.1) {
 				std::cout << "Bad total rate ratio: " << ratio << std::endl;
 				exit(1);
 			}
 
-			ratio = float(consumed_[3] + consumed_[4]) / (sub_limiter_[0].limit(0) * duration);
+			ratio = float(consumed_[3] + consumed_[4]) / (sub_limiter_[0].limit(fz::direction::inbound) * duration);
 			if (ratio < 0.9 || ratio > 1.1) {
 				std::cout << "Bad sublimit rate ratio: " << ratio << std::endl;
 				exit(1);
@@ -73,14 +73,14 @@ struct handler : public fz::event_handler
 
 		for (size_t i = 0; i < buckets_.size(); ++i) {
 			size_t amount = rates_[i];
-			size_t available = buckets_[i].available(0);
-			if (available != fz::unlimited && available < amount) {
+			size_t available = buckets_[i].available(fz::direction::inbound);
+			if (available != fz::rate::unlimited && available < amount) {
 				amount = available;
 			}
 			if (elapsed >= delay) {
 				consumed_[i] += amount;
 			}
-			buckets_[i].consume(0, amount);
+			buckets_[i].consume(fz::direction::inbound, amount);
 		}
 	}
 
