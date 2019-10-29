2221ed53564c383675d3262590e58886a7c494ed
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-10-29 08:50:53 +0000

    Use std::apply for ordinary functors now that we require C++17

--- lib/libfilezilla/apply.hpp
+++ lib/libfilezilla/apply.hpp
@@ -17,36 +17,6 @@
 namespace fz {
 
 /** \private
- * Apply tuple to ordinary functor
- */
-template<typename F, typename Tuple, size_t... I>
-auto apply_(F&& f, Tuple&& t, std::index_sequence<I...> const&) -> decltype(std::forward<F>(f)(std::get<I>(std::forward<Tuple>(t))...))
-{
-	return std::forward<F>(f)(std::get<I>(std::forward<Tuple>(t))...);
-}
-
-/**
- * \brief Apply tuple to ordinary functor
- *
- * Example:
- *
- * \code
- *
- * int f(int value, std::string const& str) { return value + str.size(); }
- *
- * auto t = std::make_tuple(5, std::string("Hello!"));
- *
- * int v = fz::apply(&f, t);
- * // v is now 11
- * \endcode
- */
-template<typename F, typename Tuple, typename Seq = typename std::make_index_sequence<std::tuple_size<typename std::remove_reference<Tuple>::type>::value>>
-auto apply(F && f, Tuple&& args) -> decltype(apply_(std::forward<F>(f), std::forward<Tuple>(args), Seq()))
-{
-	return apply_(std::forward<F>(f), std::forward<Tuple>(args), Seq());
-}
-
-/** \private
  * Apply tuple to pointer to member
  */
 template<typename Obj, typename F, typename Tuple, size_t... I>
