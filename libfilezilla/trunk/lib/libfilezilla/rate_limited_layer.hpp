910998e046eb1a7a00f662d0ed52fd82f375411f
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2021-01-27 14:29:47 +0000

    Add compound_rate_limited_layer to which limiters can be added and removed dynamically

--- libfilezilla/trunk/lib/libfilezilla/rate_limited_layer.hpp
+++ libfilezilla/trunk/lib/libfilezilla/rate_limited_layer.hpp
@@ -45,6 +45,49 @@ protected:
 	virtual void wakeup(direction::type d) override;
 };
 
+/**
+ * \brief A compound rate-limited socket layer.
+ *
+ * A socket layer to which any number of \sa rate_limiter can be added and removed dynamically
+ */
+class FZ_PUBLIC_SYMBOL compound_rate_limited_layer final : public socket_layer
+{
+public:
+	compound_rate_limited_layer(event_handler* handler, socket_interface& next_layer);
+	virtual ~compound_rate_limited_layer();
+
+	void add_limiter(rate_limiter * limiter);
+	void remove_limiter(rate_limiter * limiter);
+
+	virtual int read(void* buffer, unsigned int size, int& error) override;
+	virtual int write(void const* buffer, unsigned int size, int& error) override;
+
+	virtual socket_state get_state() const override {
+		return next_layer_.get_state();
+	}
+
+	virtual int connect(native_string const& host, unsigned int port, address_type family = address_type::unknown) override {
+		return next_layer_.connect(host, port, family);
+	}
+
+	virtual int shutdown() override {
+		return next_layer_.shutdown();
+	}
+
+	virtual void set_event_handler(event_handler* handler) override {
+		scoped_lock l(mtx_);
+		socket_layer::set_event_handler(handler);
+	}
+
+protected:
+	class crll_bucket;
+	friend class crll_bucket;
+	std::vector<std::unique_ptr<crll_bucket>> buckets_;
+
+	fz::mutex mtx_{false};
+};
+
+
 }
 
 #endif
