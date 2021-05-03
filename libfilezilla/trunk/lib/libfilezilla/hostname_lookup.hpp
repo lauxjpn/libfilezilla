ab863b192d63d891c67d9a6e4f0877771f020561
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2021-05-03 08:06:53 +0000

    Add documentation

--- libfilezilla/trunk/lib/libfilezilla/hostname_lookup.hpp
+++ libfilezilla/trunk/lib/libfilezilla/hostname_lookup.hpp
@@ -1,6 +1,10 @@
 #ifndef LIBFILEZILLA_HOSTNAME_LOOKUP_HEADER
 #define LIBFILEZILLA_HOSTNAME_LOOKUP_HEADER
 
+/** \file
+ * \brief Header for the \ref fz::hostname_lookup class
+ */
+
 #include "libfilezilla.hpp"
 #include "iputils.hpp"
 #include "event_handler.hpp"
@@ -12,6 +16,14 @@ public:
 	hostname_lookup(thread_pool& pool, event_handler& evt_handler);
 	~hostname_lookup();
 
+	/**
+	 * \brief Looks up the passed host
+	 *
+	 * If family is unknown, looks up both IPv4 and IPv6 addresses, if the operating system
+	 * is configured to resolve IPv6 addresses.
+	 *
+	 * If the function returns true, wait for the \ref hostname_lookup_event before you can call it again.
+	 */
 	bool lookup(native_string const& host, address_type family = address_type::unknown);
 
 private:
@@ -22,6 +34,7 @@ private:
 /// \private
 struct hostname_lookup_event_type {};
 
+/// Results of hostname_lookup. On success, second argument is zero, otherwise an error code.
 typedef simple_event<hostname_lookup_event_type, hostname_lookup*, int, std::vector<std::string>> hostname_lookup_event;
 }
 
