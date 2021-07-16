51bb400cd31adaf27343ba297da4ba95cd8462b1
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2021-07-16 10:36:08 +0000

    Use std::variant, add parameter for pretty-print

--- lib/libfilezilla/json.hpp
+++ lib/libfilezilla/json.hpp
@@ -4,6 +4,7 @@
 #include "string.hpp"
 
 #include <map>
+#include <variant>
 
 namespace fz {
 
@@ -17,84 +18,85 @@ enum class json_type {
 	boolean
 };
 
-class FZ_PUBLIC_SYMBOL json
+class FZ_PUBLIC_SYMBOL json final
 {
 public:
 	json() noexcept = default;
+	json(json const&) = default;
+	json(json &&) noexcept = default;
 
 	explicit json(json_type t)
-	    : type_(t)
-	{}
+	{
+		set_type(t);
+	}
 
 	json_type type() const {
 		return type_;
 	}
 
 	std::string string_value() const {
-		return type_ == json_type::string ? value_ : "";
+		return type_ == json_type::string ? std::get<0>(value_) : "";
 	}
 
 	template<typename T>
 	T number_value() const {
-		return type_ == json_type::number ? to_integral<T>(value_) : T{};
+		return type_ == json_type::number ? to_integral<T>(std::get<0>(value_)) : T{};
 	}
 
-	void erase(std::string const& name) {
-		children_.erase(name);
+	bool bool_value() const {
+		return type_ == json_type::boolean ? std::get<3>(value_) : false;
 	}
-	json& operator[](std::string const& name) {
-		if (type_ == json_type::none) {
-			type_ = json_type::object;
-		}
-		return children_[name];
-	}
+
+	void erase(std::string const& name);
 
 	json const& operator[](std::string const& name) const;
+	json& operator[](std::string const& name);
 
-	bool set(std::string const& name, json const& j);
-	bool set(std::string const& name, std::string_view const& s);
-	template<typename Bool, std::enable_if_t<std::is_same_v<bool, typename std::decay_t<Bool>>, int> = 0>
-	bool set(std::string const& name, Bool b)
-	{
-		json j(json_type::boolean);
-		j.value_ = b ? "true" : "false";
-		return set(name, j);
-	}
+	json const& operator[](size_t i) const;
+	json& operator[](size_t i);
 
-	bool set(std::string const& name, int64_t v);
-	bool set(std::string const& name, uint64_t v);
-
-	bool append(json const& j);
-	bool append(std::string_view const& s);
-	bool append(int64_t v);
-	bool append(uint64_t v);
+	size_t children() const;
 
 	template<typename Bool, std::enable_if_t<std::is_same_v<bool, typename std::decay_t<Bool>>, int> = 0>
 	json& operator=(Bool b) {
-		clear();
 		type_ = json_type::boolean;
-		value_ = b ? "true" : "false";
+		value_ = b;
+		return *this;
+	}
+
+	json& operator=(uint64_t n) {
+		type_ = json_type::number;
+		value_ = fz::to_string(n);
+		return *this;
+	}
+	json& operator=(int64_t n) {
+		type_ = json_type::number;
+		value_ = fz::to_string(n);
 		return *this;
 	}
 
 	json& operator=(std::string_view const& v);
 
+	json& operator=(json const&) = default;
+	json& operator=(json &&) noexcept = default;
+
 	explicit operator bool() const { return type_ != json_type::none; }
 
-	std::string to_string() const;
+	std::string to_string(bool pretty = false, size_t depth = 0) const;
 
-	static json parse(std::string_view const& v);
+	static json parse(std::string_view const& v, size_t max_depth = 20);
 
 	void clear();
 
 private:
-	static json parse(char const*& p, char const* end, int depth);
-	json_type type_{json_type::none};
+	bool check_type(json_type t);
+	void set_type(json_type t);
 
-	// todo variant
-	std::map<std::string, json, std::less<>> children_;
-	std::vector<json> entries_;
-	std::string value_;
+	static json parse(char const*& p, char const* end, size_t max_depth);
+
+	typedef std::variant<std::string, std::map<std::string, json, std::less<>>, std::vector<json>, bool> value_type;
+	value_type value_;
+	json_type type_{json_type::none};
 };
 }
 
